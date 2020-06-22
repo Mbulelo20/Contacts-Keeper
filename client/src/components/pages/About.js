9e89ce65daae993a854a977040d4ce2ae8668521
mbulelo20 <mbulelopani09@gmail.com>
2020-06-22 18:01:52 +0200

    second project

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div>
+            <h1>
+                About the app
+            </h1>
+            <p className="my-1">
+                This is one my very first full stack apps
+            </p>
+            <p className="bg-dark p">
+                <strong>Version</strong> 1.0.0
+            </p>
+        </div>
+    )
+}
+export default About
